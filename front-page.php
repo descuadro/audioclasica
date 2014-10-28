9a9bbd5300e4a647a7af0d558262204229cf54c4
Gorka Molero <gorka.molero@gmail.com>
2014-10-27 19:21:03 -0500

    initial front-page commit

--- front-page.php
+++ front-page.php
@@ -5,39 +5,28 @@
  * @package themeHandle
  */
 
+$post_classes = array(
+	'story',
+);
+
 get_header(); ?>
 
-main role="main" class="home">
-
-	<!-- Hola Negro! -->
-	<!-- Cómo va? -->
-	<!-- Todo bien gracias -->
-	<!-- No tan bien, verás -->
-	<!-- Por? Qué onda? -->
-	<!-- No se, te veo desmejorado -->
-	<!-- Se me está cayendo el pelo... -->
-	<!-- Ya veo. Te llaman Samuel L. Jackson -->
+<main role="main" class="home">
 
 <section class="home-feed feed">
 	<?php while ( have_posts() ) : the_post(); ?>
-		<article class="story story-1 tendencias">
+		<article id="post-<?php the_ID(); ?>" <?php post_class( $post_classes ); ?> class="story tendencias">
 		    <header>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<h1 class="entry-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+						<h6 class="entry-details">
+							<span class="entry-author"><?php the_author() ?></span>
+							<a href="#" class="category"><?php the_category() ?></a>
+						</h6>
 					</header><!-- .entry-header -->
-		
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-<?php the_ID(); ?> -->
+				</article>
 			</header>
-		</article>
-
-	<style>
-		.feed .story-1 { background: url('images/story1.fb6a.jpg'); }
-	</style>
+		</article><!-- #post-<?php the_ID(); ?> -->
 	<?php endwhile; // end of the loop. ?>
 </section>
 
