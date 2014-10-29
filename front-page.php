91406c9e333f92855c49a8af6b0bd80dd93bf9ce
Gorka Molero <gorka.molero@gmail.com>
2014-10-29 12:04:58 -0500

    fix

--- front-page.php
+++ front-page.php
@@ -13,11 +13,23 @@ get_header(); ?>
 	<?php /* Start the Loop */ ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
+		<?php
+			if ( has_post_thumbnail() ) {
+         $thumbnail = get_the_post_thumbnail($post->ID,'thumbnail');
+    	}
+		?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class( $post_classes ); ?>>
 
-			<div class="image">
-				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-    	</div>
+			<?php if ( $image_based ): ?>
+				<style> #post-<?php the_ID(); ?> { background-image: url('<?php echo $thumbnail; ?>'); } </style>
+
+			<?php else: ?>
+
+				<?php if (  has_post_thumbnail() ): ?>
+					<div class="image"><?php the_post_thumbnail(); ?></div>
+				<?php endif ?>
+
+			<?php endif ?>
 
 			<header class="entry-header">
 				<h1 class="story-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
